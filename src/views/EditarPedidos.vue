47204dc22746d11b103c1085f965fd81953fb1b9
Dimas Capelari <dimas.capelari@gmail.com>
2022-07-20 22:23:10 -0300

    tabela

--- src/views/EditarPedidos.vue
+++ src/views/EditarPedidos.vue
@@ -1,47 +1,80 @@
 <template>
   <div class="container">
     <h1>Editar Pedidos</h1>
-    <form id="pedido-form">
-      <div class="input-container">
-        <p><strong>id:</strong> <input type="text" v-model="pedido.id" /></p>
-        <p>
-          <strong>Código EAN:</strong>
-          <input type="text" v-model="pedido.ean" />
-        </p>
-        <p>
-          <strong>Código Interno:</strong>
-          <input type="text" v-model="pedido.codigointerno" />
-        </p>
-        <p>
-          <strong>Descrição:</strong>
-          <input type="text" v-model="pedido.descricao" />
-        </p>
-        <p>
-          <strong>Detalhamento:</strong>
-          <input type="text" v-model="pedido.detalhamento" />
-        </p>
-        <p>
-          <strong>Fabricante:</strong>
-          <input type="text" v-model="pedido.fabricante" />
-        </p>
-        <p><strong>Cor:</strong> <input type="text" v-model="pedido.cor" /></p>
-        <p>
-          <strong>Tamanho:</strong>
-          <input type="text" v-model="pedido.tamanho" />
-        </p>
-        <p>
-          <strong>Preço:</strong> <input type="text" v-model="pedido.preco" />
-        </p>
-      </div>
-      <div class="input-container">
-        <input
-          type="button"
-          @click="atualizaPedido"
-          class="submit-btn"
-          value="Atualizar!"
-        />
-      </div>
-    </form>
+    <section class="form-container">
+      <form id="pedido-form">
+        <div class="input-container">
+          <p><strong>id:</strong> <input type="text" v-model="pedido.id" /></p>
+          <p>
+            <strong>Código EAN:</strong>
+            <input type="text" v-model="pedido.ean" />
+          </p>
+          <p>
+            <strong>Código Interno:</strong>
+            <input type="text" v-model="pedido.codigointerno" />
+          </p>
+          <p>
+            <strong>Descrição:</strong>
+            <input type="text" v-model="pedido.descricao" />
+          </p>
+          <p>
+            <strong>Detalhamento:</strong>
+            <input type="text" v-model="pedido.detalhamento" />
+          </p>
+          <p>
+            <strong>Fabricante:</strong>
+            <select v-model="pedido.fabricante">
+              <template v-for="fabricante in fabricantes" :key="fabricante.id">
+                <option :value="fabricante.codmarc">
+                  {{ fabricante.tipo }}
+                </option>
+              </template>
+            </select>
+          </p>
+
+          <p>
+            <strong>Cor:</strong>
+            <select v-model="pedido.cor">
+              <template v-for="cor in cores" :key="cor.id">
+                <option :value="cor.codcor">
+                  {{ cor.tipo }}
+                </option>
+              </template>
+            </select>
+          </p>
+          <p>
+            <strong>Tamanho:</strong>
+            <select v-model="pedido.tamanho">
+              <template v-for="tamanho in tamanhos" :key="tamanho.id">
+                <option :value="tamanho.tipo">
+                  {{ tamanho.tipo }}
+                </option>
+              </template>
+            </select>
+            <!-- <input type="text" v-model="pedido.tamanho" /> -->
+          </p>
+          <p>
+            <strong>Preço:</strong> <input type="text" v-model="pedido.preco" />
+          </p>
+        </div>
+        <div class="input-container">
+          <input
+            type="button"
+            @click="atualizaPedido"
+            class="submit-btn"
+            value="Atualizar!"
+          />
+        </div>
+      </form>
+    </section>
+    <!-- teste -------------------------- -->
+
+    <!-- <ul>
+      <li v-for="ped in this.pedido" :key="ped.id">
+        <p>{{ ped }}</p>
+      </li>
+    </ul> -->
+    <!-- -------------------------------- -->
   </div>
 </template>
 
@@ -63,14 +96,40 @@ export default {
         tamanho: "",
         preco: "",
       },
+      fabricantes: [],
+      cores: [],
+      tamanhos: [],
     };
   },
 
   mounted() {
     console.log(typeof this.$route.params.id);
     this.getPedidos();
+    this.getFabricantes();
+    this.getCores();
+    this.getTamanhos();
   },
+
   methods: {
+    getTamanhos() {
+      axios.get("http://localhost:3000/tenis").then((response) => {
+        this.tamanhos = response.data.tamanhos;
+      });
+    },
+
+    getCores() {
+      axios.get("http://localhost:3000/tenis").then((response) => {
+        this.cores = response.data.cores;
+      });
+    },
+
+    getFabricantes() {
+      axios.get("http://localhost:3000/tenis").then((response) => {
+        this.fabricantes = response.data.fabricantes;
+        console.log(this.fabricantes.fabricantes);
+      });
+    },
+
     getPedidos() {
       axios.get("http://localhost:3000/pedidos").then((response) => {
         this.pedido = response.data.find(
@@ -96,6 +155,16 @@ export default {
 <style scoped>
 .container {
   height: 400px;
+}
+
+.container h1 {
+  margin-top: 30px;
+}
+
+.form-container {
+  display: flex;
+  justify-content: center;
+  gap: 100px;
 }
 
 .input-container {
@@ -124,5 +193,10 @@ export default {
 .submit-btn:hover {
   background-color: transparent;
   color: #222;
+}
+
+select {
+  width: 153px;
+  margin: 3px;
 }
 </style>
